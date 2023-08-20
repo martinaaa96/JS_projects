6737e1bf53306945980dcfff24b3f454b09a079b
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-20 14:34:44 +0300

    add setHeader to allow headers

--- server/controlles/userController.js
+++ server/controlles/userController.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
 
-router.post('/register',(req,res)=>{
-res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
-res.setHeader('Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, PATCH, DELETE');
+router.post('/register', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers',
+        'Content-Type, Authorization');
 
 
 });
 
 
-router.post('/login',(req,res)=>{
+router.post('/login', (req, res) => {
 
 });
 
-router.get('/logout',(req,res)=>{
+router.get('/logout', (req, res) => {
 
 });
 
