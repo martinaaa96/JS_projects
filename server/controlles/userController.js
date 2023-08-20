0ce510c4d18d2ce36e43ba765686dea209ac919c
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-20 14:33:36 +0300

    add setHeader for allow methods

--- server/controlles/userController.js
+++ server/controlles/userController.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 
 router.post('/register',(req,res)=>{
 res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+res.setHeader('Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, PATCH, DELETE');
+
+
 });
+
 
 router.post('/login',(req,res)=>{
 
