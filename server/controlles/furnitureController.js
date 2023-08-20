0ad0f5c07cb39aa2424c16be4449632990a37810
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-20 17:01:56 +0300

    details furniture

--- server/controlles/furnitureController.js
+++ server/controlles/furnitureController.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.json({ ok: true });
+const furnitureManager = require('../managers/furnitureManager');
+
+router.get('/',async (req, res) => {
+    const furnitures = await furnitureManager.getAll();
+
+    res.json(furnitures);
 
 });
 
+router.get('/:furnitureId',async (req,res)=>{
+    const furniture = await furnitureManager.getOne(req.params.furnitureId);
 
-router.post('/',(req,res)=>{
+    res.json(furniture);
+    
+});
+
+router.post('/', async (req, res) => {
     const furnitureData = req.body;
 
-    
-})
+    await furnitureManager.create(furnitureData);
+
+    res.json({ _id: furnitureData._id });
+
+});
 
 module.exports = router;
